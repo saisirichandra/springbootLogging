02b322afac5a5b294748ba498ec142589e02bac6
saisirichandra <sirichandrapodugu@gmail.com>
2021-05-12 14:53:33 +0530

    springbootLogging

---
+++ springboot-logging/src/test/java/com/siri/springboot2/springboot2logging/SpringbootLoggingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siri.springboot2.springboot2logging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootLoggingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
